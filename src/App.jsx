e500907c6150b4787d83653d0c69d92330d10944
s3xmaxman <badside3919@gmail.com>
2024-10-08 18:04:20 +0900

    build(deps): update package-lock.json, package.json and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./sections/Header";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main className="overflow-hidden">
+      <Header />
+    </main>
   );
 };
 
