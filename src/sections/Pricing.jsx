0a8f032047fd0546e1d6b27403f6f4c9b98cbe3c
s3xmaxman <badside3919@gmail.com>
2024-10-09 20:52:16 +0900

    update Pricing.jsx

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -152,7 +152,7 @@ const Pricing = () => {
 
                 {index === 1 && (
                   <p className="small-compact mt-9 text-center text-p3 before:mx-2.5 before:content-['-'] after:mx-2.5 after:content-['-']">
-                    Limited time offer
+                    期間限定のオファー
                   </p>
                 )}
               </div>
