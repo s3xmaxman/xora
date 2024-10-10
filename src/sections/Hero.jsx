c720656fd5a74374b8d2210f49e2879cadfc95c6
s3xmaxman <badside3919@gmail.com>
2024-10-10 12:38:52 +0900

    update src/constants/index.jsx and Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,6 +11,8 @@ const Hero = () => {
               Video Editing
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
+              AI動画編集
+              <br />
               驚くほど簡単
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
